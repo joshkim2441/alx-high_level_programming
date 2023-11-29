47d28521b6d4eb792be4ef801f8b6c1e5d679bc1
joshkim2441 <joshkim2441@gmail.com>
2023-11-30 02:14:37 +0300

    Task 4

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-
-
     import hidden_4
-    name = dir(hidden_4)
-    for i in range(len(name)):
-        if name[i][0] != "_" and [i][1] != "_":
-            print(name[i])
+    for name in sorted(dir(hidden_4)):
+        if name[:2] != "__":
+            print("{}".format(name))
