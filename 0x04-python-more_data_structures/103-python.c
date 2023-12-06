5e4d6b1bf7c266b5e6469780df0f669da4b87541
joshkim2441 <joshkim2441@gmail.com>
2023-12-06 14:20:00 +0300

    "0x04-python-more_data_structures"

--- 0x04-python-more_data_structures/103-python.c
+++ 0x04-python-more_data_structures/103-python.c
@@ -5,46 +5,48 @@
 
 void print_python_list(PyObject *p)
 {
-	long int size, i;
-	PyObject *item;
+	long int size = PyList_Size(p);
+	int i;
+	PyListObject *list = (PyListObject *)p;
+	const char *type;
 
-	print("[*] Python list info\n");
-	if (!PyList_Check(p))
-	{
-		printf("  [ERROR] Invalid List Object\n");
-		return;
-	}
-
-	size = PyList_Size(p);
-	printf("[*] Size of the Python List = %ld\n", size);
+	printf("[*] Python list info\n");
+	printf("[*] Size of the Python List = %li\n", size);
+	printf("[*] Allocated = %li\n", list->allocated);
 
 	for (i = 0; i < size; i++)
 	{
-		item = PyList_GetItem(p, i);
-		printf("Element %ld: %s\n", i, Py_TYPE(item)->tp_name);
+		type = (list->ob_item[i])->ob_type->tp_name;
+		printf("Element %i: %s\n", i, type);
+		if (!strcmp(type, "bytes"))
+			print_python_bytes(list->ob_item[i]);
 	}
 }
 
 void print_python_bytes(PyObject *p)
 {
-	char *str;
-	long int size, i;
+	char *str = NULL;
+	long int size;
+	int i;
 
-	printf("[*] Python bytes\n");
+	printf("[.] bytes object info\n");
 	if (!PyBytes_Check(p))
 	{
 		printf("  [ERROR] Invalid Bytes Object\n");
 		return;
 	}
 
-	str = PyBytes_AsString(p);
-	size = PyBytes_Size(p);
-	printf("  Size of the Python Bytes = %ld\n", size);
-	printf("  Trying string: %s\n", str);
-	printf("  First 10 bytes: ");
+	PyBytes_AsStringAndSize(p, &str, &size);
+
+	printf("  size: %li\n", size);
+	printf("  trying string: %s\n", str);
+	if (size < 10)
+		printf("  first %li bytes:", str + 1);
+	else
+		printf("  first 10 bytes:");
 	for (i = 0; i < size && i < 10; i++)
 	{
-		printf("%02hhx ", str[i]);
+		printf(" %02hhx", str[i]);
 	}
 	printf("\n");
 }
