f9cd97375c18aea951eb4d3e7eca57efe995b2f9
joshkim2441 <joshkim2441@gmail.com>
2023-12-06 13:50:45 +0300

    Tasks

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,7 +1,3 @@
 #!/usr/bin/python3
 def square_matrix_map(matrix=[]):
-    """ Return a new matrix of same size, where each vlaue is the
-    square of the corresponding value in the matrix
-    Initial matrix is not modified
-    """
     return list(map(lambda row: list(map(lambda x: x ** 2, row)), matrix))
