c52ed244a9035b7807893bdc05d3789e9a24b4e6
joshkim2441 <joshkim2441@gmail.com>
2023-12-18 20:09:55 +0300

    Task 6

--- 0x05-python-exceptions/6-raise_exception_msg.py
+++ 0x05-python-exceptions/6-raise_exception_msg.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
 
 def raise_exception_msg(message=""):
-    try:
-        raise_exception_msg("This is a name exception.")
-    except NameError as e:
-        print("Caught an exception: ", e)
+    raise NameError(message)
