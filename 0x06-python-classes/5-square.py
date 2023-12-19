076c7b23006df8ee05bf56a6262c48b5bc04b69a
joshkim2441 <joshkim2441@gmail.com>
2023-12-19 22:49:45 +0300

    Task 5

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -1,35 +1,51 @@
 #!/usr/bin/python3
-"""define square with private instance: size"""
+"""define a class that defines a Square"""
 
 
 class Square:
-    """instantiate an object, set the size
-    during creation of a square object
-    """
+    """a class Square that defines a square"""
     def __init__(self, size=0):
+        """initiate a square
+
+        Args:
+            value (int): the size of the square
+        """
         self.__size = size
-        """get the value of size with type
-        and value checks"""
+
     @property
     def size(self):
+    """initiate: Private size
+
+    Returns:
+        Private size
+    """
         return self.__size
-    """set the value of size with type
-    and value checks"""
+
     @size.setter
     def size(self, value):
+        """sets a value into size, it must be an int
+
+        Args:
+            value (int): the size of the square
+        """
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
         elif value < 0:
             raise ValueError("size must be >= 0")
         else:
             self.__size * self.__size
-            """return the area of the square"""
+
     def area(self):
+        """Returns the area
+
+        Returns:
+            area
+        """
         return self.__size * self.__size
-    """print the square using character # or
-    empty line if size is 0
-    """
+
     def my_print(self):
+        """prints the square with the character # in stdout"""
+
         if self.__size == 0:
             print()
         else:
