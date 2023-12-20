0ae8060ae83241f92483015c871da2fffef6c92a
joshkim2441 <joshkim2441@gmail.com>
2023-12-20 15:48:38 +0300

    Task 7

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -56,6 +56,7 @@ class SinglyLinkedList:
         """
         new_node = Node(value)
         if self.__head is None:
+            new_node.next_node = None
             self.__head = new_node
         elif value < self.__head.data:
             new_node.next_node = self.__head
