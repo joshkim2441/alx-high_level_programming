151399429a615da842fd0b664f2a5ad1d835fd3c
joshkim2441 <joshkim2441@gmail.com>
2024-03-12 01:33:50 +0300

    JavaScript warmup

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,11 +1,9 @@
 #!/usr/bin/node
 const x = Math.floor(Number(process.argv[2]));
 if (!isNaN(x)) {
-  // Argument can be converted to an integer
 	for (let i = 0; i < x; i++) {
 		console.log('C is fun');
 	}
 } else {
-  // Argumnet cannot be converted to an integer
 	console.log('Missing number of ocurrences');
 }
