4bb5ad087078dc16f8ae6c658290a2c6c3c5f543
joshkim2441 <joshkim2441@gmail.com>
2024-03-12 00:56:34 +0300

    JavaScript warmup

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 // Check if any arguments were passed
 if (process.argv.length > 2) {
-        // Print the first argument
-        console.log(process.argv[2]);
+  // Print the first argument
+  console.log(process.argv[2]);
 } else {
-        // No arguments passed, print "No argument"
-        console.log("No argument");
+  // No arguments passed, print "No argument"
+  console.log("No argument");
 }
--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -3,5 +3,5 @@
 const langs = ["c is fun", "Python is cool", "JavaScript is amazing"];
 // Loop through the the array and print each line
 for (const lang of langs) {
-        console.log(lang);
+  console.log(lang);
 }
