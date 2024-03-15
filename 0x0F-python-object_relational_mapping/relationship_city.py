5996224aa6993374fa5b2dfd3f0a89f350970d46
joshkim2441 <joshkim2441@gmail.com>
2024-03-16 00:08:28 +0300

    Python ORM

--- 0x0F-python-object_relational_mapping/relationship_city.py
+++ 0x0F-python-object_relational_mapping/relationship_city.py
@@ -14,6 +14,6 @@ class City(Base):
     """
 
     __tablename__ = "cities"
-    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
+    id = Column(Integer, primary_key=True)
     name = Column(String(128), nullable=False)
-    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
+    state_id = Column(Integer, ForeignKey('states.id'))
