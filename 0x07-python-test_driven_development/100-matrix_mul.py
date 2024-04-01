adb20fe21f56134e6ed6d0b3826502243eeb0d86
joshkim2441 <joshkim2441@gmail.com>
2024-04-01 20:08:12 +0300

    Python tests

--- 0x07-python-test_driven_development/100-matrix_mul.py
+++ 0x07-python-test_driven_development/100-matrix_mul.py
@@ -1,55 +1,95 @@
 #!/usr/bin/python3
 """
-Defines the matrix_mul function
+
+Module composed by a function that multiplies 2 matrices
+
 """
 
 
 def matrix_mul(m_a, m_b):
-    """Multiply two matrices(lists of lists of ints and floats)"""
-    if type(m_a) is not list:
+    """ Function that multiplies 2 matrices
+
+    Args:
+        m_a: matrix a
+        m_b: matrix b
+
+    Returns:
+        result of the multiplication
+
+    Raises:
+        TypeError: if m_a or m_b aren't a list
+        TypeError: if m_a or m_b aren't a list of a lists
+        ValueError: if m_a or m_b are empty
+        TypeError: if the lists of m_a or m_b don't have integers or floats
+        TypeError: if the rows of m_a or m_b don't have the same size
+        ValueError: if m_a and m_b can't be multiplied
+
+
+    """
+
+    if not isinstance(m_a, list):
         raise TypeError("m_a must be a list")
-    list1 = len(m_a)
-    if list1 == 0:
-        raise ValueError("m_a can't be empty")
-    list2 = None
-    for a in m_a:
-        if type(a) is not list:
-            raise TypeError("m_a must be a list of lists")
-        if list2 is None:
-            list2 = len(a)
-            if list2 == 0:
-                raise ValueError("m_a can't be empty")
-        if list2 != len(a):
-            raise TypeError("each row of m_a must be of the same size")
-        for b in a:
-            if type(b) is not int and type(b) is not float:
-                raise TypeError("m_a should contain only integers or floats")
-    if type(m_b) is not list:
+
+    if not isinstance(m_b, list):
         raise TypeError("m_b must be a list")
-    if len(m_b) == 0:
-        raise ValueError("m_b can't be empty")
-    list3 = None
-    for a in m_b:
-        if type(a) is not list:
+
+    for elems in m_a:
+        if not isinstance(elems, list):
+            raise TypeError("m_a must be a list of lists")
+
+    for elems in m_b:
+        if not isinstance(elems, list):
             raise TypeError("m_b must be a list of lists")
-        if list3 is None:
-            list3 = len(a)
-            if list3 == 0:
-                raise ValueError("m_b can't be empty")
-        if list3 != len(a):
-            raise TypeError("each row of m_b must be of the same size")
-        for b in a:
-            if type(b) is not int and type(b) is not float:
+
+    if len(m_a) == 0 or (len(m_a) == 1 and len(m_a[0]) == 0):
+        raise ValueError("m_a can't be empty")
+
+    if len(m_b) == 0 or (len(m_b) == 1 and len(m_b[0]) == 0):
+        raise ValueError("m_b can't be empty")
+
+    for lists in m_a:
+        for elems in lists:
+            if not type(elems) in (int, float):
+                raise TypeError("m_a should contain only integers or floats")
+
+    for lists in m_b:
+        for elems in lists:
+            if not type(elems) in (int, float):
                 raise TypeError("m_b should contain only integers or floats")
-    if list2 != len(m_b):
+
+    length = 0
+
+    for elems in m_a:
+        if length != 0 and length != len(elems):
+            raise TypeError("each row of m_a must be of the same size")
+        length = len(elems)
+
+    length = 0
+
+    for elems in m_b:
+        if length != 0 and length != len(elems):
+            raise TypeError("each row of m_b must be of the same size")
+        length = len(elems)
+
+    if len(m_a[0]) != len(m_b):
         raise ValueError("m_a and m_b can't be multiplied")
-    matrix = []
-    for a in range(list1):
-        c = []
-        for b in range(list3):
-            m = 0
-            for d in range(list2):
-                m += m_a[a][d] * m_b[d][b]
-            c.append(m)
-        matrix.append(c)
-    return matrix
+
+    r1 = []
+    i1 = 0
+
+    for a in m_a:
+        r2 = []
+        i2 = 0
+        num = 0
+        while (i2 < len(m_b[0])):
+            num += a[i1] * m_b[i1][i2]
+            if i1 == len(m_b) - 1:
+                i1 = 0
+                i2 += 1
+                r2.append(num)
+                num = 0
+            else:
+                i1 += 1
+        r1.append(r2)
+
+    return r1
