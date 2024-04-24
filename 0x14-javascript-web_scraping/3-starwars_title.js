0cd518e9dc335b9dc4802b0068e204bf0657ae82
joshkim2441 <joshkim2441@gmail.com>
2024-04-24 05:47:06 +0300

    js web scraping

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -5,9 +5,9 @@
 const request = require('request');
 const endPoint = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
 request({ url: endPoint, methods: 'GET' }, function (err, response, body) {
-    if (err) {
-        console.log(err);
-    } else {
-        console.log(body && JSON.parse(body).title);
-    }
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(body && JSON.parse(body).title);
+  }
 });
