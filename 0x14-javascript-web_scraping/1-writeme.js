7908f1cc7e3f380f22bd8c4b7b3b67e41d0a13bb
joshkim2441 <joshkim2441@gmail.com>
2024-04-24 05:51:04 +0300

    js web scraping

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -2,19 +2,8 @@
 // Writes a string to a file
 
 const fs = require('fs');
-
-// The first argument is the file path
-const filePath = process.argv[2];
-
-// The second argument is the string to write
-const content = process.argv[3];
-fs.writeFile(filePath, content, 'utf8',
-  function (err) {
-    if (err) {
-      // If an error occurred during writing,
-      // print the error object
-      console.log(err);
-    } else {
-      console.log('File written successfully!');
-    }
-  });
+fs.writeFile(process.argv[2], process.argv[3], err => {
+  if (err) {
+    console.log(err);
+  }
+});
